5ed29be8d662a31b8d7f15cc97be9ab42a1a3ddf
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-01 18:05:43 -0400

    add: login api call

---
+++ src/controllers/userController.ts
@@ -0,0 +1,28 @@
+const BASE_URL = (await import.meta.env.PUBLIC_API_URL) as string;
+
+export const loginUser = async (email: string, password: string) => {
+  let response = await fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password, //TODO: encrypt before sending
+    }),
+  });
+  response = await response.json();
+  if (response.message === "error") {
+    console.error(response.data);
+  }
+
+  return response;
+};
+
+export const registerUser = (
+  email: string,
+  username: string,
+  password: string
+) => {
+  console.log("Register");
+};
