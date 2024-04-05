e820b4b9928f0a77b5b694492e51a8970c29e67e
Mikadifo <mikadifopadillaheredia@gmail.com>
2024-04-05 19:45:49 -0400

    add: transactions create fetch controller

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -1,3 +1,5 @@
+import type { Transaction } from "../models/transaction";
+
 const BASE_URL = (await import.meta.env.PUBLIC_API_URL) as string;
 
 export const getUserTransactions = (
@@ -9,5 +11,23 @@ export const getUserTransactions = (
     headers: {
       "access-token": token,
     },
+  });
+};
+
+export const createTransactions = (
+  token: string,
+  bankId: string,
+  transactions: Transaction[]
+): Promise<Response> => {
+  return fetch(`${BASE_URL}/transactions}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "access-token": token,
+    },
+    body: JSON.stringify({
+      bankId,
+      transactions: JSON.stringify(transactions),
+    }),
   });
 };
